443e172a187e394c565f019c1443f2e4f391edbc
Alejandro R. Mosteo <amosteo@unizar.es>
2026-06-19 16:26:05 +0200

    Move to newer ESM

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,8 +7,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const core = require('@actions/core');
-const { dockerCommand } = require('docker-cli-js');
+import * as core from '@actions/core';
+import { dockerCommand } from 'docker-cli-js';
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const { dockerCommand } = require('docker-cli-js');
+import * as core from '@actions/core';
+import { dockerCommand } from 'docker-cli-js';
 
 async function run() {
    try {
